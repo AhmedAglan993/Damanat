0557b6aa4e1570210b6a00d17ad5fbe9ae032d61
ahmedaglan1 <97390857+ahmedaglan1@users.noreply.github.com>
2025-07-08 18:17:04 +0300

    hotspot cieling

--- Assets/Scripts/HotSpots/HotSpot.cs
+++ Assets/Scripts/HotSpots/HotSpot.cs
@@ -7,22 +7,39 @@ using UnityEngine;
 public class HotSpot : MonoBehaviour
 {
     [SerializeField] public string hotSpotName;
+
+    [Header("Optional")]
+    [SerializeField] private bool hasCeiling = false;
+    [SerializeField] private GameObject ceilingObject;
+
     void Start()
     {
         GetComponentInChildren<TextMeshProUGUI>().text = hotSpotName;
         GetComponentInChildren<CleanButton>().onClick.AddListener(() =>
         {
             GetComponentInChildren<StatisticsPopupOpener>().OpenPopup();
-            print(GetComponent<HotSpot>().hotSpotName);
             OverallStatisticsUIController.Instance.SetIsInRoom(true);
-            RoomStatistics roomStatistics = new RoomStatistics(GetComponent<HotSpot>().hotSpotName);
-            GetComponentInChildren<StatisticsPopupOpener>().popup.GetComponent<RoomStatisticsUIController>().ShowStatistics(roomStatistics);
+            RoomStatistics stats = new RoomStatistics(hotSpotName);
+            GetComponentInChildren<StatisticsPopupOpener>().popup.GetComponent<RoomStatisticsUIController>().ShowStatistics(stats);
+            foreach (HotSpot hotSpot in UIManager.Instance.CurrenthotSpotsHolder.HotSpots){
+                hotSpot.ToggleCeiling(true);
+            }
+            // 🔻 Hide ceiling if needed
+            if (hasCeiling && ceilingObject != null)
+                ceilingObject.SetActive(false);
         });
     }
+
     [ContextMenu("SetHotspotNames")]
     public void SetHotspotNames()
     {
         gameObject.name = hotSpotName;
     }
 
+    public void ToggleCeiling(bool enable)
+    {
+        if (hasCeiling && ceilingObject != null)
+            ceilingObject.SetActive(enable);
+    }
 }
+
